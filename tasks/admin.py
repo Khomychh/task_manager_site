30e1eaa6aa613a645173709c7b64ef0bd2ce6e80
Dean LTS <seminarydeansync@gmail.com>
2025-10-28 20:06:57 +0200

    registration models in admin

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from tasks.models import Task, Worker, Position, Team, Project
+
+
+@admin.register(Worker)
+class WorkerAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Position)
+class PositionAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    pass
