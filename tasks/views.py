ed790a29b8e247e2018f1a99b9bb47614635c192
Dean LTS <seminarydeansync@gmail.com>
2025-10-28 16:02:07 +0200

    feet: add links to tasks/projects/workers and improve counters in home page

--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,18 @@
+from multiprocessing.pool import worker
+
 from django.shortcuts import render
 
-from tasks.models import Task
+from tasks.models import Task, Worker, Project
 
 
 def index(request):
     tasks_count = Task.objects.count()
-    context = {"tasks_count": tasks_count}
+    project_count = Project.objects.all()
+    worker_count = Worker.objects.all()
+
+    context = {
+        "tasks_count": tasks_count,
+        "project_count": project_count,
+        "worker_count": worker_count,
+    }
     return render(request, "index.html", context=context)
