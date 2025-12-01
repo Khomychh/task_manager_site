28826d3d6560a7cf1638c170d1eb1f53b9b3a577
Ivan Khomych <ivan.khomychh@gmail.com>
2025-12-01 20:36:27 +0200

    security: rotate SECRET_KEY and move to env

--- task_manager_site/settings.py
+++ task_manager_site/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,8 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-f8row=&$x)!pw&j5)7$1znc=!kd@hcng9ce&kfo_vb+_6iwjj5"
+BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / ".env")
+
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+DEBUG = os.environ.get("DEBUG", "0") == "1"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
